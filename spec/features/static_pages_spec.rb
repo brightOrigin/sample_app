452b655e5e9fdd17de3858aaff382f8a3576b80a
Tony Papale <tony@fashiongps.com>
2014-03-07 11:35:48 -0800

    Add a title helper

--- spec/features/static_pages_spec.rb
+++ spec/features/static_pages_spec.rb
@@ -11,10 +11,17 @@ describe "Static pages" do
 		end		
 
 
-		it "should have the right title" do
+		it "should have the base title" do
 
 			visit '/static_pages/home'
-			page.should have_title('Ruby on Rails Tutorial Sample App | Home')
+			page.should have_title('Ruby on Rails Tutorial Sample App')
+
+		end
+
+		it "should not have a cutom page title" do
+
+			visit '/static_pages/home'
+			page.should_not have_title('| Home')
 
 		end
 
