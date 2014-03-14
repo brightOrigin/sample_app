87fe3b7f352e627e8bae100c35834b10be9597fd
Tony Papale <tony@fashiongps.com>
2014-03-14 15:52:00 -0700

    Finish updating user model

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,6 +1,6 @@
 module SessionsHelper
 
-  def sign_in(user)
+  def  sign_in(user)
     remember_token = User.new_remember_token
     cookies.permanent[:remember_token] = remember_token
     user.update_attribute(:remember_token, User.hash(remember_token))
@@ -36,6 +36,15 @@ module SessionsHelper
                                   User.hash(User.new_remember_token))
     cookies.delete(:remember_token)
     self.current_user = nil
+  end
+
+  def redirect_back_or(default)
+    redirect_to(session[:return_to] || default)
+    session.delete(:return_to)
+  end
+
+  def store_location
+    session[:return_to] = request.url if request.get?
   end
 
 end
